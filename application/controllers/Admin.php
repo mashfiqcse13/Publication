b15be734ecc2e3ac7645879bac5d8cf2cf349e0e
rokibul <rokibulhasan.356@gmail.com>
2015-08-13 00:31:31 +0600

    update url

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -12,6 +12,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
  *
  * @author MD. Mashfiq
  */
+
+//define('DASHBOARD', "$baseurl");
 define('BOOK_MANAGEMENT', 'index.php/Admin/manage_book');
 define('CONTACT_MANAGEMENT', 'index.php/Admin/manage_contact');
 define('STOCK_MANAGEMENT', 'index.php/Admin/manage_stock');
@@ -24,12 +26,24 @@ class Admin extends CI_Controller{
     
     function index(){
         $data['theme_asset_url'] = base_url().THEME_ASSET;
+        $data['base_url']=base_url();
         $this->load->view('Admin_theme/AdminLTE/dashboard',$data);
     }
     
     function manage_book(){
         $data['theme_asset_url'] = base_url().THEME_ASSET;
+        $data['base_url']=base_url();
         $this->load->view('Admin_theme/AdminLTE/manage_book',$data);
+    }
+    
+        function manage_contact(){
+        $data['theme_asset_url'] = base_url().THEME_ASSET;
+        $this->load->view('Admin_theme/AdminLTE/manage_contact',$data);
+    }
+    
+        function manage_stock(){
+        $data['theme_asset_url'] = base_url().THEME_ASSET;
+        $this->load->view('Admin_theme/AdminLTE/manage_stock',$data);
     }
             
     function memo(){
