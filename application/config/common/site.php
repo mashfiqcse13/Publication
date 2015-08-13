547da35ee857ae10cb760e7b7f846e8c7a7c248b
mashfiqcse13 <mashfiqnahid@gmail.com>
2015-08-13 10:41:00 -0700

    Adding config file
    I have added config file and autoloaded it .

---
+++ application/config/common/site.php
@@ -0,0 +1,12 @@
+<?php
+
+/* 
+ * To change this license header]=choose License Headers in Project Properties.
+ * To change this template file]=choose Tools | Templates
+ * and open the template in the editor.
+ */
+$config['BOOK_MANAGEMENT']='index.php/Admin/manage_book';
+$config['CONTACT_MANAGEMENT']='index.php/Admin/manage_contact';
+$config['STOCK_MANAGEMENT']='index.php/Admin/manage_stock';
+$config['PAYMENT_MANAGEMENT']='index.php/Admin/manage_payment';
+$config['THEME_ASSET']='asset/Admin_theme/AdminLTE/';
--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -14,35 +14,39 @@ defined('BASEPATH') OR exit('No direct script access allowed');
  */
 
 //define('DASHBOARD', "$baseurl");
-define('BOOK_MANAGEMENT', 'index.php/Admin/manage_book');
-define('CONTACT_MANAGEMENT', 'index.php/Admin/manage_contact');
-define('STOCK_MANAGEMENT', 'index.php/Admin/manage_stock');
-define('PAYMENT_MANAGEMENT', 'index.php/Admin/manage_payment');
-define('THEME_ASSET', 'asset/Admin_theme/AdminLTE/');
+
 
 
 class Admin extends CI_Controller{
     //put your code here
     
     function index(){
-        $data['theme_asset_url'] = base_url().THEME_ASSET;
+        $data['theme_asset_url'] = base_url(). $this->config->item('THEME_ASSET');
         $data['base_url']=base_url();
+        $data['Title']='Dashboard';
         $this->load->view('Admin_theme/AdminLTE/dashboard',$data);
     }
     
     function manage_book(){
-        $data['theme_asset_url'] = base_url().THEME_ASSET;
+        $data['theme_asset_url'] = base_url().$this->config->item('THEME_ASSET');
         $data['base_url']=base_url();
+        $data['Title']='Dashboard';
         $this->load->view('Admin_theme/AdminLTE/manage_book',$data);
     }
     
         function manage_contact(){
-        $data['theme_asset_url'] = base_url().THEME_ASSET;
+        $data['theme_asset_url'] = base_url().$this->config->item('THEME_ASSET');
+        $data['Title']='Dashboard';
+        
+        
         $this->load->view('Admin_theme/AdminLTE/manage_contact',$data);
     }
     
         function manage_stock(){
-        $data['theme_asset_url'] = base_url().THEME_ASSET;
+        $data['theme_asset_url'] = base_url().$this->config->item('THEME_ASSET');
+        $data['Title']='Dashboard';
+        
+        
         $this->load->view('Admin_theme/AdminLTE/manage_stock',$data);
     }
             
