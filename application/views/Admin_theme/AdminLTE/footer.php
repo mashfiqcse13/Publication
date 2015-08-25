2e79b780dad8bd48436653cff7c64c171c24964c
rokibul <rokibulhasan.356@gmail.com>
2015-08-25 09:25:36 +0600

    solve dashboard error

--- application/views/Admin_theme/AdminLTE/footer.php
+++ application/views/Admin_theme/AdminLTE/footer.php
@@ -20,13 +20,13 @@
     <!-- jQuery UI 1.11.4 -->
     <script src="https://code.jquery.com/ui/1.11.4/jquery-ui.min.js" type="text/javascript"></script>
     <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
-
+<?php if(isset($glosary)): ?>
         <!-- glosary crud js file -->
           <?php foreach($glosary->js_files as $file): ?>
          
             <script src="<?php echo $file; ?>"></script>
         <?php endforeach; ?>
-
+<?php endif; ?>
         
     <script type="text/javascript">
       $.widget.bridge('uibutton', $.ui.button);
