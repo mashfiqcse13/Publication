a1282e3fbbbd8dbb24c39a32bf5ec56106e65b7a
mashfiqcse13 <mashfiqnahid@gmail.com>
2015-09-30 17:09:31 +0600

    Menu updated

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -1,4 +1,5 @@
 <?php
+
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 /*
@@ -12,17 +13,17 @@ defined('BASEPATH') OR exit('No direct script access allowed');
  *
  * @author MD. Mashfiq
  */
-
 //define('DASHBOARD', "$baseurl");
 
 
 
-class Admin extends CI_Controller{
+class Admin extends CI_Controller {
+
     //put your code here
     function __construct() {
         parent::__construct();
         $this->load->library('tank_auth');
-        
+
         if (!$this->tank_auth->is_logged_in()) {         //not logged in
             redirect('login');
             return 0;
@@ -30,40 +31,40 @@ class Admin extends CI_Controller{
 
         $this->load->library('grocery_CRUD');
     }
-    
-    function index(){
+
+    function index() {
 
         $this->load->model('account/account');
-        $data['account_today']=$this->account->today();
-        $data['account_monthly']=$this->account->monthly();
-        $data['total']=$this->account->total();
-        
-        $data['theme_asset_url'] = base_url(). $this->config->item('THEME_ASSET');
-        $data['base_url']=base_url();
-        $data['Title']='Dashboard';
-        $this->load->view($this->config->item('ADMIN_THEME').'dashboard',$data);
+        $data['account_today'] = $this->account->today();
+        $data['account_monthly'] = $this->account->monthly();
+        $data['total'] = $this->account->total();
+
+        $data['theme_asset_url'] = base_url() . $this->config->item('THEME_ASSET');
+        $data['base_url'] = base_url();
+        $data['Title'] = 'Dashboard';
+        $this->load->view($this->config->item('ADMIN_THEME') . 'dashboard', $data);
     }
-    
-    function manage_book(){
-        
+
+    function manage_book() {
+
         $crud = new grocery_CRUD();
         $crud->set_table('pub_books')->set_subject('Book');
         $crud->callback_add_field('catagory', function () {
-            return form_dropdown('catagory', $this->config->item('book_categories'),'0');
+            return form_dropdown('catagory', $this->config->item('book_categories'), '0');
         });
         $crud->callback_add_field('storing_place', function () {
             return form_dropdown('storing_place', $this->config->item('storing_place'));
         });
-        $output =  $crud->render();
+        $output = $crud->render();
         $data['glosary'] = $output;
 
-        $data['theme_asset_url'] = base_url().$this->config->item('THEME_ASSET');
-        $data['base_url']=base_url();
-        $data['Title']='Manage Book';
-        $this->load->view($this->config->item('ADMIN_THEME').'manage_book',$data);
+        $data['theme_asset_url'] = base_url() . $this->config->item('THEME_ASSET');
+        $data['base_url'] = base_url();
+        $data['Title'] = 'Manage Book';
+        $this->load->view($this->config->item('ADMIN_THEME') . 'manage_book', $data);
     }
-    
-    function manage_contact(){
+
+    function manage_contact() {
 
         $crud = new grocery_CRUD();
         $crud->set_table('pub_contacts')->set_subject('Contact');
@@ -71,42 +72,40 @@ class Admin extends CI_Controller{
             return form_dropdown('contact_type', $this->config->item('contact_type'));
         });
         $crud->callback_edit_field('contact_type', function ($value, $primary_key) {
-            return form_dropdown('contact_type', $this->config->item('contact_type'),$value);
+            return form_dropdown('contact_type', $this->config->item('contact_type'), $value);
         });
-        $output =  $crud->render();
+        $output = $crud->render();
         $data['glosary'] = $output;
 
 
 
-        $data['theme_asset_url'] = base_url().$this->config->item('THEME_ASSET');
-        $data['base_url']=base_url();
-        $data['Title']='Manage Contact';
-        
-        
-        $this->load->view($this->config->item('ADMIN_THEME').'manage_contact',$data);
+        $data['theme_asset_url'] = base_url() . $this->config->item('THEME_ASSET');
+        $data['base_url'] = base_url();
+        $data['Title'] = 'Manage Contact';
+
+
+        $this->load->view($this->config->item('ADMIN_THEME') . 'manage_contact', $data);
     }
 
+    function manage_stock() {
 
-    function manage_stock(){
-        
         $crud = new grocery_CRUD();
         $crud->set_table('pub_stock')->set_subject('Stock');
-        
-        $crud->set_relation('book_ID','pub_books','name');
-        $crud->set_relation('printing_press_ID','pub_contacts','name');
-        $crud->set_relation('binding_store_ID','pub_contacts','name');
-        $crud->set_relation('sales_store_ID','pub_contacts','name');
-        $output =  $crud->render();
+
+        $crud->set_relation('book_ID', 'pub_books', 'name');
+        $crud->set_relation('printing_press_ID', 'pub_contacts', 'name');
+        $crud->set_relation('binding_store_ID', 'pub_contacts', 'name');
+        $crud->set_relation('sales_store_ID', 'pub_contacts', 'name');
+        $output = $crud->render();
         $data['glosary'] = $output;
 
-        $data['theme_asset_url'] = base_url().$this->config->item('THEME_ASSET');
-        $data['base_url']=base_url();
-        $data['Title']='Manage Book';
-        $this->load->view($this->config->item('ADMIN_THEME').'stock_manage',$data);
+        $data['theme_asset_url'] = base_url() . $this->config->item('THEME_ASSET');
+        $data['base_url'] = base_url();
+        $data['Title'] = 'Manage Book';
+        $this->load->view($this->config->item('ADMIN_THEME') . 'stock_manage', $data);
     }
-    
 
-    function account(){
+    function account() {
 //        $this->load->model('custom/stock_manage');
 
         $this->load->model('account/account');
@@ -118,154 +117,147 @@ class Admin extends CI_Controller{
         // $data['total_bank_due']=$this->account->total_bank_due();
         // $data['total_due']=$this->account->total_due();
         // $data['total_sell']=$this->account->totalsell();
-        $data['account_today']=$this->account->today();
-        $data['account_monthly']=$this->account->monthly();
-        $data['total']=$this->account->total();
-        $data['theme_asset_url'] = base_url().$this->config->item('THEME_ASSET');
-        $data['Title']='Account Information';
-        $data['base_url']=base_url();
-        
-        $this->load->view($this->config->item('ADMIN_THEME').'account',$data);
+        $data['account_today'] = $this->account->today();
+        $data['account_monthly'] = $this->account->monthly();
+        $data['total'] = $this->account->total();
+        $data['theme_asset_url'] = base_url() . $this->config->item('THEME_ASSET');
+        $data['Title'] = 'Account Information';
+        $data['base_url'] = base_url();
 
+        $this->load->view($this->config->item('ADMIN_THEME') . 'account', $data);
     }
 
-
-
-
-    function memo($momo_id=1){
+    function memo($momo_id = 1) {
         $this->load->model('Memo');
-        
-        $data['theme_asset_url'] = base_url().$this->config->item('THEME_ASSET');
-        $data['Title']='Memo Generation';
-        $data['base_url']=base_url();
-        
-        $data['Book_selection_table']=$this->Memo->memogenerat($momo_id);
+
+        $data['theme_asset_url'] = base_url() . $this->config->item('THEME_ASSET');
+        $data['Title'] = 'Memo Generation';
+        $data['base_url'] = base_url();
+
+        $data['Book_selection_table'] = $this->Memo->memogenerat($momo_id);
 
         //$data['memo']=$this->Memo->memogenerat();
         //var_dump($data['memo']);
         //$this->Memo->memogenerat();
-        
-        $this->load->view($this->config->item('ADMIN_THEME').'memo',$data);
+
+        $this->load->view($this->config->item('ADMIN_THEME') . 'memo', $data);
     }
 
-     function memo_management(){
+    function memo_management() {
         $crud = new grocery_CRUD();
 
         $crud->set_table('pub_memos')
                 ->set_subject('Memo')
-                ->display_as('contact_ID','Party Name');
-        $crud->set_relation('contact_ID','pub_contacts','name');
+                ->display_as('contact_ID', 'Party Name');
+        $crud->set_relation('contact_ID', 'pub_contacts', 'name');
         $crud->callback_add_field('memo_serial', function () {
-            $unique_id=uniqid();
-            return '<label>'.$unique_id.'</label><input type="hidden" maxlength="50" value="'.$unique_id.'" name="memo_serial" >';
+            $unique_id = uniqid();
+            return '<label>' . $unique_id . '</label><input type="hidden" maxlength="50" value="' . $unique_id . '" name="memo_serial" >';
         });
-        $crud->callback_add_field('sub_total', array($this,'add_book_selector_table'));
-        $crud->callback_edit_field('sub_total', array($this,'edit_book_selector_table'));
-        
+        $crud->callback_add_field('sub_total', array($this, 'add_book_selector_table'));
+        $crud->callback_edit_field('sub_total', array($this, 'edit_book_selector_table'));
 
-        $this->grocery_crud->callback_add_field('issue_date',array($this,'_date_fill_now'));
-   
+
+        $this->grocery_crud->callback_add_field('issue_date', array($this, '_date_fill_now'));
+
 
         $crud->callback_after_insert(array($this, 'after_adding_memo'));
-        
+
 
         $output = $crud->render();
-        
+
 //        $this->grocery_crud->set_table('pub_memos')->set_subject('Memo');
 //        $output =  $this->grocery_crud->render();
         $data['glosary'] = $output;
 
-        $data['theme_asset_url'] = base_url().$this->config->item('THEME_ASSET');
-        $data['Title']='Memo Management';
-        $data['base_url']=base_url();
-        
-        $this->load->view($this->config->item('ADMIN_THEME').'memo_management',$data);
+        $data['theme_asset_url'] = base_url() . $this->config->item('THEME_ASSET');
+        $data['Title'] = 'Memo Management';
+        $data['base_url'] = base_url();
+
+        $this->load->view($this->config->item('ADMIN_THEME') . 'memo_management', $data);
     }
 
-protected function _date_fill_now()
-{
-        return '<input name="date" type="text" value="'. current_time() . '" maxlength="19" class="datetime-input">';
-}
+    protected function _date_fill_now() {
+        return '<input name="date" type="text" value="' . current_time() . '" maxlength="19" class="datetime-input">';
+    }
 
     function add_book_selector_table() {
-            $this->load->library('table');
-            // Getting Data
-            $query = $this->db->query("SELECT * FROM `pub_books`");
-            $data=array();
-            foreach ($query->result_array() as $index => $row ){
-                array_push($data, [$row['name'],
-                            $row['price'],
-                             '<input style="width: 100px;" data-index="'.$index.'" data-price="'.$row['price'].'" name="quantity['.$row['book_ID'].']" value="0" class="numeric form-control" type="number">'
-                    . '     <input name="price['.$row['book_ID'].']" value="'.$row['price'].'" type="hidden">'
-                            ]);
-            }
-            $this->table->set_heading('Book Name', 'Price', 'Quantity');
+        $this->load->library('table');
+        // Getting Data
+        $query = $this->db->query("SELECT * FROM `pub_books`");
+        $data = array();
+        foreach ($query->result_array() as $index => $row) {
+            array_push($data, [$row['name'],
+                $row['price'],
+                '<input style="width: 100px;" data-index="' . $index . '" data-price="' . $row['price'] . '" name="quantity[' . $row['book_ID'] . ']" value="0" class="numeric form-control" type="number">'
+                . '     <input name="price[' . $row['book_ID'] . ']" value="' . $row['price'] . '" type="hidden">'
+            ]);
+        }
+        $this->table->set_heading('Book Name', 'Price', 'Quantity');
 
-            //Setting table template
-            $tmpl = array (
-                'table_open'  => '<table class="table table-bordered table-striped">',
-                'heading_cell_start'=>'<th class="success">'
-            );
-            $this->table->set_template($tmpl);
+        //Setting table template
+        $tmpl = array(
+            'table_open' => '<table class="table table-bordered table-striped">',
+            'heading_cell_start' => '<th class="success">'
+        );
+        $this->table->set_template($tmpl);
 
-            $output ='<label>Select Book Quantity:</label><div style="overflow-y:scroll;max-height:200px;">
-                    '.$this->table->generate($data).'</div>
+        $output = '<label>Select Book Quantity:</label><div style="overflow-y:scroll;max-height:200px;">
+                    ' . $this->table->generate($data) . '</div>
                    <label>Sub Total :</label><span id="sub_total">0</span><span>Tk</span> <input type="hidden" maxlength="50  " name="sub_total">';
-            return $output;
-        }
+        return $output;
+    }
 
-
-        
     function edit_book_selector_table($value, $primary_key) {
-            $this->load->library('table');
-            // Getting Data
-            $where = array('memo_ID' => $primary_key);
-            $book_selection_query = $this->db->get_where('pub_memos_selected_books',$where );
-            $book_selection = $book_selection_query->result_array();
-            
-            foreach ($book_selection as $index => $row ){
-                $book_ID = $row['book_ID'];
-                $quantity = $row['quantity'];
-                $book_quantity_by_id[$book_ID] = $quantity ;
-            }
-            
-            $query = $this->db->query("SELECT * FROM `pub_books`");
-            $data=array();
-            foreach ($query->result_array() as $index => $row ){
-                array_push($data, [$row['name'],
-                            $row['price'],
-                             '<input style="width: 100px;" data-index="'.$index.'" data-price="'.$row['price'].'" name="quantity['.$row['book_ID'].']" value="'.$book_quantity_by_id[$row['book_ID']].'" class="numeric form-control" type="number">'
-                    . '     <input name="price['.$row['book_ID'].']" value="'.$row['price'].'" type="hidden">'
-                            ]);
-            }
-            $this->table->set_heading('Book Name', 'Price', 'Quantity');
+        $this->load->library('table');
+        // Getting Data
+        $where = array('memo_ID' => $primary_key);
+        $book_selection_query = $this->db->get_where('pub_memos_selected_books', $where);
+        $book_selection = $book_selection_query->result_array();
 
-            //Setting table template
-            $tmpl = array (
-                'table_open'  => '<table class="table table-bordered table-striped">',
-                'heading_cell_start'=>'<th class="success">'
+        foreach ($book_selection as $index => $row) {
+            $book_ID = $row['book_ID'];
+            $quantity = $row['quantity'];
+            $book_quantity_by_id[$book_ID] = $quantity;
+        }
+
+        $query = $this->db->query("SELECT * FROM `pub_books`");
+        $data = array();
+        foreach ($query->result_array() as $index => $row) {
+            array_push($data, [$row['name'],
+                $row['price'],
+                '<input style="width: 100px;" data-index="' . $index . '" data-price="' . $row['price'] . '" name="quantity[' . $row['book_ID'] . ']" value="' . $book_quantity_by_id[$row['book_ID']] . '" class="numeric form-control" type="number">'
+                . '     <input name="price[' . $row['book_ID'] . ']" value="' . $row['price'] . '" type="hidden">'
+            ]);
+        }
+        $this->table->set_heading('Book Name', 'Price', 'Quantity');
+
+        //Setting table template
+        $tmpl = array(
+            'table_open' => '<table class="table table-bordered table-striped">',
+            'heading_cell_start' => '<th class="success">'
+        );
+        $this->table->set_template($tmpl);
+
+        $output = '<label>Select Book Quantity:</label><div style="overflow-y:scroll;max-height:200px;">
+                    ' . $this->table->generate($data) . '</div>
+                   <label>Sub Total :</label><span id="sub_total">' . $value . '</span><span>Tk</span> <input type="hidden" maxlength="50" value="' . $value . '" name="sub_total">';
+        return $output;
+    }
+
+    function after_adding_memo($post_array, $primary_key) {
+        foreach ($post_array['quantity'] as $index => $value) {
+            $book_ordered_quantity_insert = array(
+                "memo_ID" => $primary_key,
+                "book_ID" => $index,
+                "quantity" => $value,
+                "price_per_book" => $post_array['price'][$index],
+                "total" => $value * $post_array['price'][$index]
             );
-            $this->table->set_template($tmpl);
 
-            $output ='<label>Select Book Quantity:</label><div style="overflow-y:scroll;max-height:200px;">
-                    '.$this->table->generate($data).'</div>
-                   <label>Sub Total :</label><span id="sub_total">'.$value.'</span><span>Tk</span> <input type="hidden" maxlength="50" value="'.$value.'" name="sub_total">';
-            return $output;
+            $this->db->insert('pub_memos_selected_books', $book_ordered_quantity_insert);
         }
-        
-        function after_adding_memo($post_array,$primary_key){
-            foreach ($post_array['quantity'] as $index => $value){
-                $book_ordered_quantity_insert = array(
-                    "memo_ID" => $primary_key,
-                    "book_ID" => $index,
-                    "quantity" => $value,
-                    "price_per_book" => $post_array['price'][$index],
-                    "total" => $value * $post_array['price'][$index]
-                );
-    
-                $this->db->insert('pub_memos_selected_books',$book_ordered_quantity_insert);
-            }
-            return TRUE;
-        }
+        return TRUE;
+    }
 
 }
