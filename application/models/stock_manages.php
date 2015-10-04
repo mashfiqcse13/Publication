157ab2619f5f097d7d6f539796b4b504d9b5d23f
mashfiqcse13 <mashfiqnahid@gmail.com>
2015-10-04 18:38:52 +0600

    latest updated

---
+++ application/models/stock_manages.php
@@ -0,0 +1,10 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+
+class Stock_Manages extends CI_Model{
+
+    function printing_press(){
+        $query="SELECT book_ID,printing_press_Quantity,binding_store_ID,binding_store_Quantity,sales_store_id,Sales_store_Quantity FROM pub_stock WHERE "
+    }
+
+}
