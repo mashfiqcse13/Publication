d49f63932f2225f352eb44d0e31377db4ec32deb
mashfiqcse13 <mashfiqnahid@gmail.com>
2015-10-11 12:45:41 +0600

    Edit Button

--- application/views/Admin_theme/AdminLTE/memo.php
+++ application/views/Admin_theme/AdminLTE/memo.php
@@ -7,56 +7,56 @@
 
 
 
-      
+
 
 <div class="container memo_print_option" style="background:#fff;width:750px;padding:25px 100px;margin-top:30px;" >
-        <div class="row">
-            <h1 class="page-header">দি যমুনা পাবলিশার্স</h1>
-        </div>
-        <div class="row">
-            <div class="pull-right">
-                <p>Date:<?php echo " " . date("Y-m-d") ?></p>
-                <p></p>
-            </div>
-        </div>
-        <div class="row">
-            <div class="pull-left">
-                <h3>দি যমুনা পাবলিশার্স</h3>
-                <p>৩৮,বাংলাবাজার(১ম তলা) <br>
-                    ঢাকা-১১০০ <br>
-                    ফোন- ৭১১৬০৬৯ <br>
-                    সাধারন তথ্য :- ০১৭১১-৮৯৮৮৮৭ <br>
-                    ব্যবসায়িক তথ্য :- ০১৭১৯-৭০৫৫৬৮ <br>
-                    বিক্রয় কেন্দ্র :- ০১৭২৮-৮৪৮৫২৩</p> <br>
-            </div>
-            <div class="pull-right">
-                <h3><?=$Book_selection_table['party_name']?></h3>
-                <p><?=$Book_selection_table['phone'] ?><br>
-                   <?=$Book_selection_table['address']?><br>
-                    <?=$Book_selection_table['district']?><br>
-                </p>
-            </div>
-        </div>
-        <div class="row">
-
-            <?= $Book_selection_table['table'] ?>
-        
-        </div>
-        <div class="row">
-            <br><br><br>
-            <p>-----------------</p>
-            <p>&nbsp;&nbsp;Signature</p>
-        
-        </div>
-        <div class="margin-top-10">
-            <a href="<?=$base_url ?>" class="only_print btn btn-primary "><i class="fa fa-dashboard"></i> Go Dashboard</a>
-       <input class="only_print pull-right btn btn-primary" type="button"  onClick="window.print()"  value="Print This Page"/>
-         <a href="" class="only_print pull-right btn btn-primary margin-10">Edit</a>
-        </div>
-        
-
-        
+    <div class="row">
+        <h1 class="page-header">দি যমুনা পাবলিশার্স</h1>
     </div>
+    <div class="row">
+        <div class="pull-right">
+            <p>Date:<?php echo " " . date("Y-m-d") ?></p>
+            <p></p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="pull-left">
+            <h3>দি যমুনা পাবলিশার্স</h3>
+            <p>৩৮,বাংলাবাজার(১ম তলা) <br>
+                ঢাকা-১১০০ <br>
+                ফোন- ৭১১৬০৬৯ <br>
+                সাধারন তথ্য :- ০১৭১১-৮৯৮৮৮৭ <br>
+                ব্যবসায়িক তথ্য :- ০১৭১৯-৭০৫৫৬৮ <br>
+                বিক্রয় কেন্দ্র :- ০১৭২৮-৮৪৮৫২৩</p> <br>
+        </div>
+        <div class="pull-right">
+            <h3><?= $Book_selection_table['party_name'] ?></h3>
+            <p><?= $Book_selection_table['phone'] ?><br>
+                <?= $Book_selection_table['address'] ?><br>
+                <?= $Book_selection_table['district'] ?><br>
+            </p>
+        </div>
+    </div>
+    <div class="row">
+
+        <?= $Book_selection_table['table'] ?>
+
+    </div>
+    <div class="row">
+        <br><br><br>
+        <p>-----------------</p>
+        <p>&nbsp;&nbsp;Signature</p>
+
+    </div>
+    <div class="margin-top-10">
+        <a href="<?= $base_url ?>" class="only_print btn btn-primary "><i class="fa fa-dashboard"></i> Go Dashboard</a>
+        <input class="only_print pull-right btn btn-primary" type="button"  onClick="window.print()"  value="Print This Page"/>
+        <a href="<?= $edit_btn_url ?>" class="only_print pull-right btn btn-primary margin-10">Edit</a>
+    </div>
+
+
+
+</div>
 
 
 
