088c9bd625703173a091fe68b586011a502f686e
MD. Mashfiq <MD. Mashfiq@DESKTOP-NVJ9RBJ>
2016-01-27 00:08:50 +0600

    Speciment Copy reduction In sold book information

--- application/models/Report.php
+++ application/models/Report.php
@@ -15,6 +15,7 @@ if (!defined('BASEPATH'))
     exit('No direct script access allowed');
 
 class Report extends CI_Model {
+    private $range ;
 
     //put your code here
     function sold_book_today($range = false) {
@@ -27,13 +28,32 @@ class Report extends CI_Model {
             $date = date('Y-m-d');
             $range = "=DATE('$date')";
         }
+        $this->range=$range;
 
-        $data = $this->db->query("SELECT name,sum(quantity) as quantity
+        $sql = "SELECT pub_memos_selected_books.book_ID,name,sum(quantity) as quantity
             FROM `pub_memos_selected_books`
             JOIN `pub_memos` ON `pub_memos_selected_books`.memo_ID = `pub_memos`.memo_ID
             JOIN `pub_books` on `pub_memos_selected_books`.`book_ID` = `pub_books`.`book_ID`
             WHERE DATE(issue_date) $range
-            GROUP BY `pub_memos_selected_books`.`book_ID`")->result_array();
+            GROUP BY `pub_memos_selected_books`.`book_ID`";
+
+        $data = $this->db->query($sql)->result_array();
+
+        $table_data = array();
+        foreach ($data as $rowIndex => $rowValue) {
+            $have_speciment_book_copy = $this->get_book_quantity($rowValue["book_ID"]);
+            if ($have_speciment_book_copy > 0) {
+                $quantity = "{$rowValue["quantity"]} - {$have_speciment_book_copy} = "
+                        . ( $rowValue["quantity"] - $have_speciment_book_copy);
+            } else {
+                $quantity = $rowValue["quantity"];
+            }
+
+            array_push($table_data, array(
+                $rowValue["name"], $quantity
+            ));
+        }
+
 //- interval 2 day
         $table_template = array(
             'table_open' => '<table class="table table-bordered table-striped move-tk-to-right-for-soldbook">',
@@ -41,24 +61,42 @@ class Report extends CI_Model {
         );
         $this->table->set_template($table_template);
 
-        $this->table->set_heading("বইয়ের নাম", "পরিমাণ");
+        $this->table->set_heading("বইয়ের নাম", array('data' => " (সর্বমোট - সৌজন্য) = বিক্রিত সংখ্যা",
+            'style' => "text-align: right;"
+        ));
 
 
-        if ($data != array())
-            return $this->table->generate($data);
-        else
+        if ($data != array()) {
+            return $this->table->generate($table_data);
+        } else {
             return "আজ কোন মেমো তৈরী হয়নি ।";
+        }
     }
 
     function dateformatter($range_string, $formate = 'Mysql') {
         $date = explode(' - ', $range_string);
-        $date[0] = explode('/', $date[0]);
+        $date[
+                0] = explode('/', $date[0]);
         $date[1] = explode('/', $date[1]);
 
         if ($formate == 'Mysql')
             return "'{$date[0][2]}-{$date[0][0]}-{$date[0][1]}' and '{$date[1][2]}-{$date[1][0]}-{$date[1][1]}'";
         else
             return $date;
+    }
+
+    function get_book_quantity($book_ID, $speciment_contact_id = FALSE) {
+        if (!$speciment_contact_id)
+            $speciment_contact_id = $this->config->item('speciment_contact_id');
+
+        $sql = "SELECT pub_memos_selected_books.book_ID,
+                        pub_memos.contact_ID,sum(quantity) as quantity
+                    FROM `pub_memos_selected_books`
+                    JOIN `pub_memos` ON `pub_memos_selected_books`.memo_ID = `pub_memos`.memo_ID
+                    WHERE pub_memos_selected_books.`book_ID`= {$book_ID} and 
+                         DATE(issue_date) {$this->range} and
+                        pub_memos.contact_ID in ({$speciment_contact_id})";
+        return $this->db->query($sql)->result_array()[0]["quantity"];
     }
 
 }
