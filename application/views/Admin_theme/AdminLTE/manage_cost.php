eb6b6a864e03c235bbd837e2400f1470c096384d
MD. Mashfiq <MD. Mashfiq@DESKTOP-NVJ9RBJ>
2016-02-16 15:28:51 +0600

    Costing View updated

--- application/views/Admin_theme/AdminLTE/manage_cost.php
+++ application/views/Admin_theme/AdminLTE/manage_cost.php
@@ -1,46 +1,45 @@
 <!--add header -->
 <?php include_once 'header.php'; ?>
 
-      <!-- Left side column. contains the logo and sidebar -->
+<!-- Left side column. contains the logo and sidebar -->
 <?php include_once 'main_sidebar.php'; ?> <!-- main sidebar area -->
-      <!-- Content Wrapper. Contains page content -->
+<!-- Content Wrapper. Contains page content -->
 <div class="content-wrapper">
-        <!-- Content Header (Page header) -->
-        <section class="content-header">
-          <h1>
-            <?=$Title ?>
-            <small> <?=$Title ?></small>
-          </h1>
-          <ol class="breadcrumb">
-            <li><a href="<?=$base_url ?>"><i class="fa fa-dashboard"></i> Dashboard</a></li>
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+        <h1>
+            <?= $Title ?>
+            <small> <?= $Title ?></small>
+        </h1>
+        <ol class="breadcrumb">
+            <li><a href="<?= $base_url ?>"><i class="fa fa-dashboard"></i> Dashboard</a></li>
             <li class="active">Cost Management</li>
-          </ol>
-        </section>
-        
-        <!-- Main content -->
-        <section class="content">
-            <div class="row">
-                <div class="col-md-12">
+        </ol>
+    </section>
 
-                    <div class="box">
-                    
-                    <?php  
+    <!-- Main content -->
+    <section class="content">
+        <div class="row">
+            <div class="col-md-12">
 
-                       echo $glosary->output;
+                <div class="box">
+
+                    <?php
+                    echo $glosary->output;
                     ?>
-               
-                    </div>
-                    
+
                 </div>
+
             </div>
-         
+        </div>
 
 
-          
-        </section><!-- /.content -->
-      </div><!-- /.content-wrapper -->
 
-      <!-- insert book -->
+
+    </section><!-- /.content -->
+</div><!-- /.content-wrapper -->
+
+<!-- insert book -->
 
 
 
