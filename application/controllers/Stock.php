6cc60741583c517053843e0e3383a7451b476b11
Sonjoy Biswas <sonjoywc@gmail.com>
2016-04-30 22:55:11 +0600

    stock controller

---
+++ application/controllers/Stock.php
@@ -0,0 +1,56 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Stock
+ *
+ * @author sonjoy
+ */
+class Stock extends CI_Controller {
+    //put your code here
+    
+    public function __construct() {
+        parent::__construct();
+        date_default_timezone_set('Asia/Dhaka');
+        $this->load->library('tank_auth');
+        if (!$this->tank_auth->is_logged_in()) {         //not logged in
+            redirect('login');
+            return 0;
+        }
+        $this->load->model('Stock_model');
+    }
+    function index(){
+        $data['theme_asset_url'] = base_url() . $this->config->item('THEME_ASSET');
+        $data['base_url'] = base_url();
+        $data['Title'] = 'Stock';
+        
+        $this->load->view($this->config->item('ADMIN_THEME').'stock/stock_dashboard', $data);
+    }
+    
+    function stock_perpetual($cmd= false){
+        $data['stock_info'] = $this->Stock_model->select_stock();
+        $data['theme_asset_url'] = base_url() . $this->config->item('THEME_ASSET');
+        $data['base_url'] = base_url();
+        $data['Title'] = 'Stock';
+        
+        $data['date_range'] = $this->input->post('date_range');
+        $date = explode('-',$data['date_range']);
+        if($data['date_range']!= ''){
+            $this->session->set_userdata('date_range', $data['date_range']);
+            $data['stock_info'] = $this->Stock_model->search_item($date[0],$date[1]);
+        }
+        if ($cmd == 'reset_date_range') {
+            $this->session->unset_userdata('date_range');
+            redirect("stock/stock_perpetual");
+        }
+        
+        
+        $this->load->view($this->config->item('ADMIN_THEME').'stock/stock_perpetual', $data);
+    }
+}
