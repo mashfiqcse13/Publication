a65afd26f9df46076e44d09734ac4080d8154711
sohel <sohel@DESKTOP-6ILJN7F>
2016-05-19 21:50:11 +0600

    update sales return page(stage 2)

--- application/controllers/Sales_return.php
+++ application/controllers/Sales_return.php
@@ -24,13 +24,16 @@ class Sales_return extends CI_Controller {
         }
         $this->load->library('grocery_CRUD');
         $this->load->model('Common');
+        $this->load->model('Sales_return_m');
+        $this->load->model('Memo');
+        
        
     }
     
     function index(){
         $data['theme_asset_url'] = base_url() . $this->config->item('THEME_ASSET');
         $data['base_url'] = base_url();
-        $data['Title'] = 'Sales Return';
+        $data['Title'] = 'Sales Return_m  ';
         
         $this->load->view($this->config->item('ADMIN_THEME').'sales_return/sales_return_dashboard', $data);
     }
@@ -38,27 +41,41 @@ class Sales_return extends CI_Controller {
     
     
     function sales_current_sales_return() {
-        $crud = new grocery_CRUD();
-        $crud->set_table('income_name');
-        $output = $crud->render();
-        $data['glosary'] = $output;
+        
+
+        
+        if($this->input->post('search_memo')==true){
+           $memo_id=$this->input->post('memo_id');
+           $data['search_memo']=$this->Sales_return_m->get_memos($memo_id);
+           $data['get_book_list']=$this->Sales_return_m->get_book_list($memo_id);
+           $data['Book_selection_table'] = $this->Memo->memogenerat($memo_id);           
+        }
+        
+        if($this->input->post('sales_return')==true){
+
+
+            $this->Sales_return_m->insert_return_item($_POST);
+            
+        }
+        
         
         $data['theme_asset_url'] = base_url() . $this->config->item('THEME_ASSET');
         $data['base_url'] = base_url();
-        $data['Title'] = 'Loan Payment';
+        $data['Title'] = 'Current Sales Return';
         $this->load->view($this->config->item('ADMIN_THEME') . 'sales_return/sales_current_sales_return', $data);
     }
     
+
     
     function sales_current_total_sales_return() {
         $crud = new grocery_CRUD();
-        $crud->set_table('income_name');
+        $crud->set_table('sales_current_total_sales_return');
         $output = $crud->render();
         $data['glosary'] = $output;
         
         $data['theme_asset_url'] = base_url() . $this->config->item('THEME_ASSET');
         $data['base_url'] = base_url();
-        $data['Title'] = 'Loan Payment';
+        $data['Title'] = 'Current Total Sales Return';
         $this->load->view($this->config->item('ADMIN_THEME') . 'sales_return/sales_current_total_sales_return', $data);
     }
     
