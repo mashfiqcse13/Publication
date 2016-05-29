8d3827cfc486c7497d5d79eeb4c54b3b44fed113
sohel <sohel@DESKTOP-6ILJN7F>
2016-05-29 20:22:08 +0600

    update CRUD

--- application/controllers/Expense.php
+++ application/controllers/Expense.php
@@ -88,8 +88,11 @@ class Expense extends CI_Controller {
         $crud->set_table('expense_name');
         $crud->display_as('id_category_expense','Expense category');
         $crud->set_relation('id_category_expense', 'expense_category', 'name_category_expense');
-        $crud->field_type('is_stock_item','enum',array('Yes','No'));
-        $crud->field_type('status_name_expense','enum',array('Yes','No'));
+        
+        $crud->change_field_type('is_stock_item', 'enum', array( 'yes' => 'y', 'no' => '2' ));
+        //$crud->field_type('status_name_expense','enum',array(1=>'Yes',2=>'No'));
+        
+        //$crud->callback_after_insert(array($this, 'add_stock_register'));
         
         $output = $crud->render();
         $data['glosary'] = $output;
@@ -100,6 +103,21 @@ class Expense extends CI_Controller {
         $this->load->view($this->config->item('ADMIN_THEME') . 'expense/expense_name', $data);
     }
     
+    function add_stock_register($postdata,$primary_key){
+        $check_stock=$this->db->where('id_name_expense',$primary_key)->get('expense_name');
+        foreach ($check_stock->result() as $result){
+            $value=$result->is_stock_item;
+        }
+        $name_expense=$postdata['name_expense'];
+        
+        if($value==1){
+            $this->db->where('id_name_expense',$name_expense);
+            $query_result=$this->db->get('stationary_stock_register');
+            if($query_result!==TRUE){
+                $this->db->query("INSERT INTO `stationary_stock_register`(`id_name_expense`) VALUES ($name_expense)");
+            }
+    }
+    }
     function expense_category() {
         $crud = new grocery_CRUD();
         $crud->set_table('expense_category');
