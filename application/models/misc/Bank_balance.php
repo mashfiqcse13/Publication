eb5c59b6ff2c5135a010aafcb9092a1c7445f799
sohel <sohel@DESKTOP-6ILJN7F>
2016-06-11 19:12:59 +0600

    bank report

--- application/models/misc/Bank_balance.php
+++ application/models/misc/Bank_balance.php
@@ -125,7 +125,60 @@ class Bank_balance extends CI_Model {
         return $query;
     }
     
-    
+    function bank_report($date){
+        $date=$this->dateformatter($date);
+        
+        $range_query=$this->db->query("SELECT name_bank,account_number,name_trnsaction_type,transaction_date,amount_transaction,username,check_number,media_link FROM `bank_management`
+LEFT JOIN bank_account on bank_account.id_bank_account=bank_management.id_account
+LEFT join bank on bank.id_bank=bank_account.id_bank
+left JOIN bank_transaction_type on bank_transaction_type.id_trnsaction_type=bank_management.id_transaction_type
+left JOIN users on users.id=bank_management.id_user 
+WHERE transaction_date BETWEEN $date");
+        
+        $this->load->library('table');
+        $this->table->set_heading(array('Bank Name', 'Account Number', 'Transaction Type','Transaction Date','Amount','User','Check Number','Media Link'));
+        $tmpl = array (
+                    'table_open'          => '<table class="table table-bordered table-striped" border="0" cellpadding="4" cellspacing="0">',
+
+                    'heading_row_start'   => '<tr>',
+                    'heading_row_end'     => '</tr>',
+                    'heading_cell_start'  => '<th>',
+                    'heading_cell_end'    => '</th>',
+
+                    'row_start'           => '<tr>',
+                    'row_end'             => '</tr>',
+                    'cell_start'          => '<td>',
+                    'cell_end'            => '</td>',
+
+                    'row_alt_start'       => '<tr>',
+                    'row_alt_end'         => '</tr>',
+                    'cell_alt_start'      => '<td>',
+                    'cell_alt_end'        => '</td>',
+
+                    'table_close'         => '</table>'
+              );
+        
+        $this->table->set_template($tmpl);
+        $this->table->set_caption('<h4><span class="pull-left">Date Range:'.$this->datereport($date).'</span><span class="pull-right">Report Date: '.date('Y-m-d h:i').'</span></h4><hr>');
+        return $this->table->generate($range_query);
+        
+        
+    }
+    function datereport($date){
+        $date= str_replace("'", ' ', $date);
+        $date=  str_replace('and', 'to', $date);
+        return $date;
+    }
+    function dateformatter($range_string, $formate = 'Mysql') {
+        $date = explode(' - ', $range_string);
+        $date[0] = explode('/', $date[0]);
+        $date[1] = explode('/', $date[1]);
+
+        if ($formate == 'Mysql')
+            return "'{$date[0][2]}-{$date[0][0]}-{$date[0][1]}' and '{$date[1][2]}-{$date[1][0]}-{$date[1][1]}'";
+        else
+            return $date;
+    }
     
    
 
