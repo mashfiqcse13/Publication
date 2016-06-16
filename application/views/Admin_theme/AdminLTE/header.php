dcad3b273c1732a8e6ae4bcae0faec1685adf0e6
sonjoy <sonjoy@DESKTOP-4A4VK8B>
2016-06-16 14:13:33 +0600

    salary and loan again update

--- application/views/Admin_theme/AdminLTE/header.php
+++ application/views/Admin_theme/AdminLTE/header.php
@@ -30,7 +30,10 @@
         <link href="<?php echo $theme_asset_url ?>plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css" rel="stylesheet" type="text/css" />
 
         <link href="<?php echo $theme_asset_url ?>plugins/select2/select2.min.css" rel="stylesheet" type="text/css" />
+        
         <link href="<?php echo $theme_asset_url ?>plugins/datatables/dataTables.bootstrap.css" rel="stylesheet" type="text/css" />
+        
+        <link href="<?php echo base_url(); ?>assets/print.css" rel="stylesheet" media="print" type="text/css" />
 
 
         <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
@@ -39,6 +42,17 @@
             <script src="<?php echo $theme_asset_url ?>https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
             <script src="<?php echo $theme_asset_url ?>https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
         <![endif]-->
+        <script type="text/javascript">
+            function printDiv(divName) {
+                var printContents = document.getElementById(divName).innerHTML;
+                var originalContents = document.body.innerHTML;
+                document.body.innerHTML = printContents;
+
+                window.print();
+
+                document.body.innerHTML = originalContents;
+            }
+        </script>
         <style type="text/css">
             .select2-container--default .select2-selection--single {
                 background-color: #fff;
