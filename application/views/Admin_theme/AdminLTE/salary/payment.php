d0540d9811c3605b5f08591fd47533657c4beec8
sonjoy <sonjoy@DESKTOP-4A4VK8B>
2016-06-26 02:08:16 +0600

    salary new

--- application/views/Admin_theme/AdminLTE/salary/payment.php
+++ application/views/Admin_theme/AdminLTE/salary/payment.php
@@ -55,7 +55,7 @@
                                             $salary = $value->amount_salary_payment;
                                             $bonus = $value->amount_salary_bonus;
                                             $total = $salary + $bonus;
-                                            $loan_installment = $value->installments_loan;
+                                            $loan_installment = $value->installments_amount_loan;
                                             $advance = $value->amount_given_salary_advance;
                                             $deduction = $loan_installment + $advance;
                                             $total = $total - $deduction;
@@ -66,7 +66,7 @@
                                                 <td><?php echo $value->amount_salary_payment; ?></td>
                                                 <td><?php echo $value->amount_salary_bonus; ?></td>
                                                 <td><?php echo $value->amount_loan; ?></td>
-                                                <td><?php echo $value->installments_loan; ?></td>
+                                                <td><?php echo $value->installments_amount_loan; ?></td>
                                                 <td><?php echo $value->amount_given_salary_advance; ?></td>
                                                 <td><?php echo $total; ?></td>
                                                 <td style="display: none;">
