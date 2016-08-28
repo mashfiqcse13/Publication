48d1038a85c94d4089a2fb375a87c86fa85b1769
sonjoy <sonjoy@DESKTOP-4A4VK8B>
2016-08-28 22:57:16 +0600

    report update 10.56pm(s)

--- application/models/Report_model.php
+++ application/models/Report_model.php
@@ -15,7 +15,7 @@ class Report_model extends CI_Model {
 
     //put your code here
     function total_sales($from, $to) {
-        $this->db->select('sum(total_amount) as total_amount,sum(total_paid) as total_paid,sum(total_due) as total_due');
+        $this->db->select('total_amount,total_paid,total_due');
         $this->db->from('sales_total_sales');
         if ($from != '') {
             $condition = "DATE(issue_date) BETWEEN '$from' AND '$to'";
@@ -24,12 +24,12 @@ class Report_model extends CI_Model {
         }
         return $this->db->get()->row();
     }
-    
-    function customer_payemnt($from, $to){
+
+    function customer_payemnt($from, $to) {
         $this->db->select('paid_amount');
         $this->db->from('customer_payment');
         $this->db->where('due_payment_status IS NULL');
-        $this->db->where('id_payment_method',1);
+        $this->db->where('id_payment_method', 1);
         if ($from != '') {
             $condition = "DATE(payment_date) BETWEEN '$from' AND '$to'";
 //            echo $condition; exit();
@@ -37,11 +37,12 @@ class Report_model extends CI_Model {
         }
         return $this->db->get()->row();
     }
-    function bank_payment($from, $to){
+
+    function bank_payment($from, $to) {
         $this->db->select('paid_amount');
         $this->db->from('customer_payment');
         $this->db->where('due_payment_status IS NULL');
-        $this->db->where('id_payment_method',3);
+        $this->db->where('id_payment_method', 3);
         if ($from != '') {
             $condition = "DATE(payment_date) BETWEEN '$from' AND '$to'";
 //            echo $condition; exit();
@@ -49,10 +50,11 @@ class Report_model extends CI_Model {
         }
         return $this->db->get()->row();
     }
-    function due_payment($from, $to){
+
+    function due_payment($from, $to) {
         $this->db->select('paid_amount');
         $this->db->from('customer_payment');
-        $this->db->where('due_payment_status',1);
+        $this->db->where('due_payment_status', 1);
         if ($from != '') {
             $condition = "DATE(payment_date) BETWEEN '$from' AND '$to'";
 //            echo $condition; exit();
@@ -60,11 +62,12 @@ class Report_model extends CI_Model {
         }
         return $this->db->get()->row();
     }
-    function advance_payment($from, $to){
+
+    function advance_payment($from, $to) {
         $this->db->select('paid_amount');
         $this->db->from('customer_payment');
         $this->db->where('due_payment_status IS NULL');
-        $this->db->where('id_payment_method',2);
+        $this->db->where('id_payment_method', 2);
         if ($from != '') {
             $condition = "DATE(payment_date) BETWEEN '$from' AND '$to'";
 //            echo $condition; exit();
@@ -72,8 +75,8 @@ class Report_model extends CI_Model {
         }
         return $this->db->get()->row();
     }
-    
-    function opening($from, $to){
+
+    function opening($from, $to) {
         $this->db->select('*');
         $this->db->from('master_reconcillation');
         if ($from != '') {
@@ -83,16 +86,97 @@ class Report_model extends CI_Model {
         }
         return $this->db->get()->row();
     }
-    function closing($from, $to){
+
+    function closing($from, $to) {
         $this->db->select('*');
         $this->db->from('master_reconcillation');
-        $this->db->order_by('id_master_reconcillation','desc');
+        $this->db->order_by('id_master_reconcillation', 'desc');
         if ($from != '') {
             $condition = "DATE(date) BETWEEN '$from' AND '$to'";
 //            echo $condition; exit();
             $this->db->where($condition);
         }
         return $this->db->get()->row();
+    }
+
+    function total_due($from, $to) {
+        $this->db->select_sum('paid_amount');
+        $this->db->from('customer_payment');
+        $this->db->where('due_payment_status', 1);
+        if ($from != '') {
+            $condition = "DATE(payment_date) BETWEEN '$from' AND '$to'";
+//            echo $condition; exit();
+            $this->db->where($condition);
+        }
+        return $this->db->get()->row();
+    }
+
+    function total_cash_collection($from, $to) {
+        $this->db->select_sum('paid_amount');
+        $this->db->from('customer_payment');
+        $this->db->where('due_payment_status IS NULL');
+        $this->db->where('id_payment_method', 1);
+        if ($from != '') {
+            $condition = "DATE(payment_date) BETWEEN '$from' AND '$to'";
+//            echo $condition; exit();
+            $this->db->where($condition);
+        }
+        return $this->db->get()->row();
+    }
+
+    function total_bank_collection($from, $to) {
+        $this->db->select_sum('paid_amount');
+        $this->db->from('customer_payment');
+        $this->db->where('due_payment_status IS NULL');
+        $this->db->where('id_payment_method', 3);
+        if ($from != '') {
+            $condition = "DATE(payment_date) BETWEEN '$from' AND '$to'";
+//            echo $condition; exit();
+            $this->db->where($condition);
+        }
+        return $this->db->get()->row();
+    }
+
+    function total_expence($from, $to) {
+        $this->db->select_sum('amount_expense');
+        $this->db->from('expense');
+        if ($from != '') {
+            $condition = "DATE(date_expense) BETWEEN '$from' AND '$to'";
+//            echo $condition; exit();
+            $this->db->where($condition);
+        }
+        return $this->db->get()->row();
+    }
+
+    function total_advance($from, $to) {
+        $this->db->select_sum('amount_paid');
+        $this->db->from('party_advance_payment_register');
+        $this->db->where('id_payment_method !=', 4);
+        if ($from != '') {
+            $condition = "DATE(date_payment) BETWEEN '$from' AND '$to'";
+//            echo $condition; exit();
+            $this->db->where($condition);
+        }
+        return $this->db->get()->row();
+    }
+
+    function total_advance_cash_bank($from, $to) {
+        $this->db->select_sum('amount_paid');
+        $this->db->from('party_advance_payment_register');
+        $this->db->where('id_payment_method', 1);
+        $this->db->or_where('id_payment_method', 3);
+        if ($from != '') {
+            $condition = "DATE(date_payment) BETWEEN '$from' AND '$to'";
+//            echo $condition; exit();
+            $this->db->where($condition);
+        }
+        return $this->db->get()->row();
+    }
+
+    function total_cash_bank($from, $to) {
+        $total_customer = $this->total_bank_collection($from, $to)->paid_amount + $this->total_cash_collection($from, $to)->paid_amount;
+        $total = $total_customer + $this->total_advance_cash_bank($from, $to)->amount_paid;
+        return $total;
     }
 
 }
