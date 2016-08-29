50d5ecb5a6d66cab47e60195309919b3b6c0b1dc
MD. Mashfiq <MD. Mashfiq@DESKTOP-NVJ9RBJ>
2016-08-30 03:10:22 +0600

    User access implemented In Due controller

--- application/controllers/Due.php
+++ application/controllers/Due.php
@@ -23,10 +23,9 @@ class Due extends CI_Controller {
         $this->load->model('Bank_model');
         $this->load->model('User_access_model');
         $super_user_id = $this->config->item('super_user_id');
-        if($super_user_id == false){
-            redirect('admin');
-        }if($super_user_id == true){
-          $this->User_access_model->if_user_has_permission($super_user_id);
+        if($super_user_id != $_SESSION['user_id'] || $this->User_access_model->if_user_has_permission(5)){
+            redirect();
+            return 0;
         }
     }
 
