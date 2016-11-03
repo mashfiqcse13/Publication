c6ffec3f28ba8ec7f564e1c24752de1697843d46
sohel <sohel@192.168.1.2>
2016-11-03 14:45:26 +0600

    sales memo edit

--- application/models/sales/Sales_edit_model.php
+++ application/models/sales/Sales_edit_model.php
@@ -15,7 +15,10 @@ class Sales_edit_model extends CI_Model {
 
     private $existing_data;     // this will be grabbed from the database
     private $changed_data;      // This will be grabbed from the edit from
-
+    private $existing_memo_data;
+    private $existing_memo_items;
+    private $changed_memo_data;
+    private $changed_memo_items;
     /*
      * This is test data section
      * Here dummy data will be added for testing purpose
@@ -23,95 +26,101 @@ class Sales_edit_model extends CI_Model {
 
     function test_data() {
 
-       $data['existing_data'] =  $this->existing_data = array(
-            'id_sales_total_sales' => 234,
-            "id_customer" => "60",
-            'item_selection' => array(
-                array(
-                    "id_item" => 1,
-                    "item_quantity" => 2,
-                    "name" => "ব্যবসায় সংগঠন ও ব্যবস্হাপনা - প্রথম পত্র",
-                    "regular_price" => "205",
-                    "sale_price" => "140",
-                    "total" => 280 // should be calculated
-                ), // should be calculated
-                array(
-                    "id_item" => 3,
-                    "item_quantity" => 5,
-                    "name" => "Business Organization & Management 2nd Paper",
-                    "regular_price" => "200",
-                    "sale_price" => "140",
-                    "total" => 280 // should be calculated
-                ), // should be calculated
-                array(
-                    "id_item" => 34,
-                    "item_quantity" => 5,
-                    "name" => "Business Organization & Management 2nd Paper",
-                    "regular_price" => "200",
-                    "sale_price" => "140",
-                    "total" => 280 // should be calculated
-                )
-            ),
-            'sub_total' => 560,
-            "discount_amount" => 0,
-            "discount_percentage" => 0,
-            'total_amount' => 560,
-            "dues_unpaid" => 0,
-            'payment_info_cash' => 210,
-            'payment_info_bank' => 210,
-            'payment_info_customer_balance_reduction' => 0,
-            'total_paid' => 461,
-            'total_due' => 101,
-            "number_of_packet" => 5,
-            'bill_for_packeting' => 31,
-            'slip_expense_amount' => 40
-        );
+      $data['existing_memo_data'] = $this->existing_memo_data =   array (
+                                    "id_total_sales" => 70,
+                                    "id_customer" => 329,
+                                    "discount_percentage" => 0,
+                                    "discount_amount" => 0,
+                                    "sub_total" => 2950,
+                                    "total_amount" => 2950,
+                                    "total_paid" => 2950,
+                                    "total_due" => 0,
+                                    "issue_date" => '2016-10-04 12:41:00',
+                                    "number_of_packet" => 0,
+                                    "bill_for_packeting" => 0,
+                                    "slip_expense_amount" => 0
+                                );
+      $data['existing_memo_items'] =  $this->existing_memo_items  = array(
+                        array(
+                               "id_sales" => 110,
+                               "id_total_sales"  => 70,
+                              "id_item" => 53,
+                               "quantity" => 5,
+                               "price" => 180,
+                               "total_cost" => 900,
+                               "discount" => 0,
+                               "sub_total" => 900
+                           ),
+                        array(
+                               "id_sales" => 110,
+                               "id_total_sales"  => 70,
+                              "id_item" => 52,
+                               "quantity" => 5,
+                               "price" => 180,
+                               "total_cost" => 900,
+                               "discount" => 0,
+                               "sub_total" => 900
+                           ),
+                        array(
+                               "id_sales" => 110,
+                               "id_total_sales"  => 70,
+                               "id_item" => 51,
+                               "quantity" => 5,
+                               "price" => 180,
+                               "total_cost" => 900,
+                               "discount" => 0,
+                               "sub_total" => 900
+                           )         
+                    );
+
        
-       $data['changed_data'] =  $this->changed_data = array(
-            'id_total_sales' => 70,
-            "id_customer" => "60",
-             'item_selection' => array(
-                array(
-                    "id_item" => 1,
-                    "item_quantity" => 7,
-                    "name" => "ব্যবসায় সংগঠন ও ব্যবস্হাপনা - প্রথম পত্র",
-                    "regular_price" => "205",
-                    "sale_price" => "140",
-                    "total" => 280 // should be calculated 'ffff
-                ), // should be calculated
-                array(
-                    "id_item" => 3,
-                    "item_quantity" => 2,
-                    "name" => "Business Organization & Management 2nd Paper",
-                    "regular_price" => "207",
-                    "sale_price" => "140",
-                    "total" => 280 // should be calculated
-                ), // should be calculated
-                array(
-                    "id_item" => 5,
-                    "item_quantity" => 2,
-                    "name" => "Business Organization & Management 2nd Paper",
-                    "regular_price" => "205",
-                    "sale_price" => "140",
-                    "total" => 280 // should be calculated
-                )
-            ),
-            'sub_total' => 560,
-            "discount_amount" => 0,
-            "discount_percentage" => 0,
-            'total_amount' => 560,
-            "dues_unpaid" => 0,
-            'payment_info_cash' => 2600,
-            'payment_info_bank' => 260,
-            'payment_info_customer_balance_reduction' => 0,
-            'total_paid' => 460,
-            'total_due' => 100,
-            "number_of_packet" => 5,
-            'bill_for_packeting' => 30,
-            'slip_expense_amount' => 40
-        );
-       
+         $data['changed_memo_data'] =  $this->changed_memo_data =  array(
+                            "id_total_sales" => 70,
+                            "id_customer" => 329,
+                            "discount_percentage" => 0,
+                            "discount_amount" => 0,
+                            "sub_total" => 2950,
+                            "total_amount" => 2950,
+                            "total_paid" => 2950,
+                            "total_due" => 0,
+                            "issue_date" => '2016-10-04 12:41:00',
+                            "number_of_packet" => 0,
+                            "bill_for_packeting" => 0,
+                            "slip_expense_amount" => 0
+                        );
         
+        $data['changed_memo_items'] =  $this->changed_memo_items  =   array(
+                array(
+                        "id_sales" => 110,
+                        "id_total_sales"  => 70,
+                       "id_item" => 53,
+                        "quantity" => 5,
+                        "price" => 180,
+                        "total_cost" => 900,
+                        "discount" => 0,
+                        "sub_total" => 900
+                    ),
+                 array(
+                        "id_sales" => 110,
+                        "id_total_sales"  => 70,
+                       "id_item" => 52,
+                        "quantity" => 5,
+                        "price" => 180,
+                        "total_cost" => 900,
+                        "discount" => 0,
+                        "sub_total" => 900
+                    ),
+                 array(
+                        "id_sales" => 110,
+                        "id_total_sales"  => 70,
+                        "id_item" => 51,
+                        "quantity" => 5,
+                        "price" => 180,
+                        "total_cost" => 900,
+                        "discount" => 0,
+                        "sub_total" => 900
+                    )
+            );
         
         return $data;
     }
@@ -119,37 +128,44 @@ class Sales_edit_model extends CI_Model {
     /*
      * This function will grabb data from the database+edit from and initialize to the respective variable
      */
-
+//
+//    function grab_data($id_sales_total_sales = null) {
+////         return $this->test_data();
+//        $sales_results = $this->db->select('*')
+//                        ->from('sales_total_sales')->get()->result();
+//        $sales = $this->db->select('*')
+//                ->from('view_customer_paid_unmarged')->get()->result();
+//        $items = $this->db->get('items')->result_array();
+//        foreach ($sales_results as $result) {
+//            $sales = $this->db->select('*')
+//                ->from('view_customer_paid_unmarged')->where('id_total_sales',$result->id_total_sales)->get()->row();
+//            $this->existing_data = array('id_sales_total_sales' => $result->id_total_sales,
+//            'id_customer' => $result->id_customer,
+//            'item_selection' => $items,
+//            'sub_total' => $result->sub_total,
+//            "discount_amount" => $result->discount_amount,
+//            "discount_percentage" => $result->discount_percentage,
+//            'total_amount' => $result->total_amount,
+//            "dues_unpaid" => $result->total_due,
+//            'payment_info_cash' => $sales->cash_paid,
+//            'payment_info_bank' => $sales->bank_paid,
+//            'payment_info_customer_balance_reduction' => $result->id_customer,
+//            'total_paid' => $result->total_paid,
+//            'total_due' => $result->total_due,
+//            "number_of_packet" => $result->number_of_packet,
+//            'bill_for_packeting' => $result->bill_for_packeting,
+//            'slip_expense_amount' => $result->slip_expense_amount);
+//        }
+////        $bank = $this->db->select('*')->from;
+//        return $this->existing_data;
+//    }
+    
     function grab_data($id_sales_total_sales = null) {
-//         return $this->test_data();
-        $sales_results = $this->db->select('*')
-                        ->from('sales_total_sales')->get()->result();
-        $sales = $this->db->select('*')
-                ->from('view_customer_paid_unmarged')->get()->result();
-        $items = $this->db->get('items')->result_array();
-        foreach ($sales_results as $result) {
-            $sales = $this->db->select('*')
-                ->from('view_customer_paid_unmarged')->where('id_total_sales',$result->id_total_sales)->get()->row();
-            $this->existing_data = array('id_sales_total_sales' => $result->id_total_sales,
-            'id_customer' => $result->id_customer,
-            'item_selection' => $items,
-            'sub_total' => $result->sub_total,
-            "discount_amount" => $result->discount_amount,
-            "discount_percentage" => $result->discount_percentage,
-            'total_amount' => $result->total_amount,
-            "dues_unpaid" => $result->total_due,
-            'payment_info_cash' => $sales->cash_paid,
-            'payment_info_bank' => $sales->bank_paid,
-            'payment_info_customer_balance_reduction' => $result->id_customer,
-            'total_paid' => $result->total_paid,
-            'total_due' => $result->total_due,
-            "number_of_packet" => $result->number_of_packet,
-            'bill_for_packeting' => $result->bill_for_packeting,
-            'slip_expense_amount' => $result->slip_expense_amount);
-        }
-//        $bank = $this->db->select('*')->from;
+        $this->existing_data['existing_memo_data'] = $this->existing_memo_data($id_sales_total_sales);
+        $this->existing_data['existing_memo_items'] = $this->existing_memo_items($id_sales_total_sales);
         return $this->existing_data;
     }
+    
 
     /*
      * This function will update the test sales table in the database
