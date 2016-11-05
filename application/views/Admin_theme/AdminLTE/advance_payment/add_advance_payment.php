ed1a76c59fd0d010147e8a50adebf1dae7c7b171
sohel <sohel@192.168.1.2>
2016-11-05 15:40:34 +0600

    due memo

--- application/views/Admin_theme/AdminLTE/advance_payment/add_advance_payment.php
+++ application/views/Admin_theme/AdminLTE/advance_payment/add_advance_payment.php
@@ -23,7 +23,7 @@
 
         <div id="block">
 
-            <div class="row" style="padding-top:0px;margin-top:14mm">
+            <div class="row" style="padding-top:0px;margin-top:14mm;margin-left:10mm;margin-right: 10mm;">
 
 
                 <?php
@@ -78,7 +78,7 @@
 
 
 
-                <div class="row" style="font-size:16px;">
+                <div class="row" style="font-size:16px;margin-left:10mm;margin-right: 10mm;">
 
 
 
