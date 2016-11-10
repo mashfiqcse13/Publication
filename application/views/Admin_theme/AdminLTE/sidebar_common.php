2884b5053734d14b24b0d3c5a55760b20707ced4
sonjoy <sonjoy@DESKTOP-4A4VK8B>
2016-11-11 00:18:19 +0600

    speciment update 12.23am(s)

--- application/views/Admin_theme/AdminLTE/sidebar_common.php
+++ application/views/Admin_theme/AdminLTE/sidebar_common.php
@@ -9,8 +9,13 @@
         $sql = "SELECT * FROM  `user_access_area` WHERE  `serial_id` != 0 and `id_user_access_area` in ($all_allowed_access_area_coma_separated) ORDER BY  `serial_id` ASC ";
     }
     $all_menu_items = $this->db->query($sql)->result();
-    foreach ($all_menu_items as $menu_item) {
-        echo "<li>" . anchor($menu_item->ci_url, $menu_item->main_menu_item_content) . "</li>";
+    $access_group = $this->db->get_where('user_access_area_permission', array('user_id'=> $_SESSION['user_id']))->row();
+    if ($access_group->id_user_access_group == '3' && $this->config->item('custom_user_menu') == 1) {
+        $this->load->view($this->config->item('ADMIN_THEME') . 'speciman_menu');
+    } else {
+        foreach ($all_menu_items as $menu_item) {
+            echo "<li>" . anchor($menu_item->ci_url, $menu_item->main_menu_item_content) . "</li>";
+        }
     }
     ?>
 <li><?php echo anchor('login/logout', '<i class="fa fa-sign-out"></i>     <span>Log Out</span>'); ?></li>
